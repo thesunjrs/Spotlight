f8ceba60fe4b27edfe0ab85825b8673b4a7e036b
Sourcery AI <>
2022-06-28 19:02:15 +0000

    'Refactored by Sourcery'

--- Spotlight_Transfer.py
+++ Spotlight_Transfer.py
@@ -6,6 +6,7 @@
 
 """
 
+
 try:
     
     import os
@@ -15,10 +16,10 @@ try:
 
     try:
         from PIL import Image
-        
+
     except ImportError:
         print ("\nInstall Pillow Image Library to run the Script")
-        
+
     src=os.path.expanduser("~")+r"\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
     src=src.replace("\\","/")
     dest=os.path.expanduser("~")+"\\Pictures\\Spotlight"
@@ -26,60 +27,67 @@ try:
 
     if not os.path.exists(dest):
         os.makedirs(dest)
-    if not os.path.exists(dest+"/Portraits"):
-        os.makedirs(dest+"/Portraits")
-        
+    if not os.path.exists(f"{dest}/Portraits"):
+        os.makedirs(f"{dest}/Portraits")
+
     count_existing=count_new=0
 
     for i in os.listdir(src):
+        current_image = f"{src}/{i}"
+        portrait = f"{dest}/Portraits/{i}"
+        landscape = f"{dest}/{i}"
 
-        current_image = src + "/" + i
-        portrait = dest + "/Portraits/" + i
-        landscape = dest + "/" + i
-        
         im = Image.open(current_image)
         width, height = im.size
-        
-        if os.path.exists(portrait) or os.path.exists(landscape) or os.path.exists(portrait+".jpg") or os.path.exists(landscape+".jpg"):
-            count_existing+=1
-        
-        else:
-            if width == 1920 or width == 1080:
-                
-                if width<height:
-                    print ("\nTransfering {} of dimensions {}x{} in {}".format(i,width,height,dest+"/Portraits"))
-                    shutil.copy2(current_image,dest+"/Portraits")
-                    print("Size :",os.path.getsize(portrait))
-                    os.rename(portrait,portrait+".jpg")
-                    
-                else:
-                    print ("\nTransfering {} of dimensions {}x{} in {}".format(i,width,height,dest))
-                    shutil.copy2(current_image,dest)
-                    print ("Size :",os.path.getsize(landscape))
-                    os.rename(landscape,landscape+".jpg")
-                    
-                count_new+=1
-                
-    else:
-        print ("*"*35 + "\n{} image(s) already exists.\n\n{} New images copied in Spotlight Directory.".format(count_existing,count_new))
-        print ("\nOperation Completed Successfully!\n"+"*"*35)
 
-        
+        if (
+            os.path.exists(portrait)
+            or os.path.exists(landscape)
+            or os.path.exists(f"{portrait}.jpg")
+            or os.path.exists(f"{landscape}.jpg")
+        ):
+            count_existing+=1
+
+        elif width in [1920, 1080]:
+                
+            if width<height:
+                print(f"\nTransfering {i} of dimensions {width}x{height} in {dest}/Portraits")
+                shutil.copy2(current_image, f"{dest}/Portraits")
+                print("Size :",os.path.getsize(portrait))
+                os.rename(portrait, f"{portrait}.jpg")
+
+            else:
+                print(f"\nTransfering {i} of dimensions {width}x{height} in {dest}")
+                shutil.copy2(current_image,dest)
+                print ("Size :",os.path.getsize(landscape))
+                os.rename(landscape, f"{landscape}.jpg")
+
+            count_new+=1
+
+    print(
+        "*" * 35
+        + f"\n{count_existing} image(s) already exists.\n\n{count_new} New images copied in Spotlight Directory."
+    )
+
+    print ("\nOperation Completed Successfully!\n"+"*"*35)
+
+
 except Exception as e:
     logs=os.path.expanduser("~")+"\\Pictures\\Spotlight\\logs.txt"
-    
+
     if os.path.exists(logs):
         f=open(logs,"a")
     else:
         f=open(logs,"a")
         f.write("="*75+"\n\nError Logs of the Wallpaper Setter by GG\n\n"+"="*75+"\n")
 
-    t="\nDate : {}\nTime : {}".format(time.strftime("%H:%M:%S",time.gmtime()),time.strftime("%d-%m-%Y",time.gmtime()))
-    
+    t = f'\nDate : {time.strftime("%H:%M:%S", time.gmtime())}\nTime : {time.strftime("%d-%m-%Y", time.gmtime())}'
+
+
     report = "="*75+"\n\n"+t+"\n\n"+"Error Message : "+str(e)+"\n\n"+"="*75+"\n"+"\n"+"="*15+"Finished"+"="*15+"\n"
 
     print (report)
-    
+
     f.write(report)
 
     f.close()
